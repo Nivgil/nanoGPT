6ddf7a3c63646a47f3a76e668e8c6009eafbafb5
nivgil <giladiniv@gmail.com>
2024-07-28 14:52:54 +0300

    Change default config for faster experiment time
    - change batch size from 8 to 16 and reduce steps by 2.

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -11,14 +11,16 @@ wandb_run_name='gpt2-124M'
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
 # 8 batch size * 1024 block size * 8 gradaccum * 8 GPUs = 524,288
-batch_size = 8
+# 16 batch size * 1024 block size * 8 gradaccum * 8 GPUs = 2 * 524,288
+batch_size = 16
 block_size = 1024
 gradient_accumulation_steps = 8 * 8
 
 # this (600_000) makes total number of tokens be 300B
 # this (200_000) makes total number of tokens be 100B
-max_iters = 200_000
-lr_decay_iters = 200_000
+# this (100_000) makes total number of tokens be 100B
+max_iters = 100_000
+lr_decay_iters = 100_000
 
 # eval stuff
 eval_interval = 1000
