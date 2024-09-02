edd0b374315157f6c408773f07b8a2b75911a9ff
nivgil <giladiniv@gmail.com>
2024-09-02 14:06:41 +0300

    Change model to raw_model for consistency. Does not change logic

--- train.py
+++ train.py
@@ -360,10 +360,10 @@ while True:
             # the official way to do this is with model.no_sync() context manager, but
             # I really dislike that this bloats the code and forces us to repeat code
             # looking at the source of that context manager, it just toggles this variable
-            model.require_backward_grad_sync = False
+            raw_model.require_backward_grad_sync = False
             require_grad_sync = (micro_step == gradient_accumulation_steps - 1)
         with ctx:
-            logits, loss = model(X, Y)
+            logits, loss = raw_model(X, Y)
             loss = loss / gradient_accumulation_steps # scale the loss to account for gradient accumulation
         # immediately async prefetch next batch while model is doing the forward pass on the GPU
         X, Y = get_batch('train')
